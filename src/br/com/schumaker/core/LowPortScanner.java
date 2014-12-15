5bb7742814990f91c913ee2dbb33cfb594e5a58a
hudson.sales <hudson.sales@SPW9712NTW7P.sondait.com.br>
2014-12-15 09:59:04 -0300

    #3

---
+++ src/br/com/schumaker/core/LowPortScanner.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.schumaker.core;
+
+/**
+ *
+ * @author hudson.sales
+ */
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class LowPortScanner {
+
+    public static void main(String[] args) {
+        String host = "localhost";
+        for (int i = 1; i < 1024; i++) {
+            try {
+                Socket s = new Socket(host, i);
+                System.out.println("There is a server on port " + i + " of " + host);
+            } catch (UnknownHostException ex) {
+                System.err.println(ex);
+                break;
+            } catch (IOException ex) {
+                System.err.println(ex);
+            }
+        }
+    }
+}
